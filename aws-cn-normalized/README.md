7517baf22f6880dcbf448e7f6a9bef172a365a8f
GeekTR <geektheripper@gmail.com>
2023-12-03 19:21:54 +0800

    aws-cn-normalized

---
+++ aws-cn-normalized/README.md
@@ -0,0 +1,11 @@
+# aws-cn-normalized
+
+This module creates missing resources in AWS China
+
+```tf
+module "normalized" { source = "github.com/linolabx/terraform-modules-aws//aws-cn-normalized" }
+```
+
+resource list:
+
+- `policy/IAMSelfManageServiceSpecificCredentials`
---
+++ aws-cn-normalized/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.29.0"
+    }
+  }
+}
+
+data "aws_partition" "this" {}
+
+resource "aws_iam_policy" "iam_self_manage_service_specific_credentials" {
+  count = data.aws_partition.this.partition == "aws-cn" ? 1 : 0
+
+  name   = "IAMSelfManageServiceSpecificCredentials"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:CreateServiceSpecificCredential",
+        "iam:ListServiceSpecificCredentials",
+        "iam:UpdateServiceSpecificCredential",
+        "iam:DeleteServiceSpecificCredential",
+        "iam:ResetServiceSpecificCredential"
+      ],
+      "Resource": "arn:aws-cn:iam::*:user/$${aws:username}"
+    }
+  ]
+}
+EOF
+}
